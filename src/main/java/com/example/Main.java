f77ed87aa9c46dd43afa488d05ad21a3d32bab58
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 10:45:47 +0200

    fine

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,31 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("Hello world!");
+        ServerSocket mioServerSocket = new ServerSocket(3000);
+        Socket mioSocket = mioServerSocket.accept();
+        System.out.println("Qualcuno si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
+
+
+        
+        String stringaRicevuta = in.readLine();
+        String stringaMaiuscola = stringaRicevuta.toUpperCase();
+        out.println(stringaMaiuscola);
+        System.out.println(stringaRicevuta);
+
+
+
+        mioServerSocket.close();
     }
 }
